28c9d2e9eaff5e93c2601617882ce0e2584def7b
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-21 12:56:35 -0700

    Add output for o

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,6 +18,8 @@ int main(void)
 	/* your code goes there */
 	if (n < 0)
 		printf("%i is negative\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
 	else
 		printf("%i is positive\n", n);
 	return (0);
