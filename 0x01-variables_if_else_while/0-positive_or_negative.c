7f856171b14ade2cfdcd5709bc80f03590b80958
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-21 12:24:47 -0700

    Create directory and file 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+  * main - This function generates a random number
+  * and then determines if it negative or positive
+  *
+  * Return: This funtion returns a value of 0
+  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+		printf("%i is negative", n);
+	else
+		printf("%i is positive", n);
+	return (0);
+}
