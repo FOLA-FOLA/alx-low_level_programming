0d4864d236153ba34c514c1e08068dcf58089b56
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-21 12:48:17 -0700

    Remove int before n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /**
@@ -16,8 +17,8 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n < 0)
-		printf("%i is negative", n);
+		printf("%i is negative\n", n);
 	else
-		printf("%i is positive", n);
+		printf("%i is positive\n", n);
 	return (0);
 }
