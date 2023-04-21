75b575d0f5aae9e623c2bf2f1d1dfa2d057b5f1a
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-20 21:08:11 -0700

    Debug printf statements for file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - prints out the size of each type
  *
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n
-		Size of an int: 4 byte(s)\n
-		Size of a long int: 4 byte(s)\n
-		Size of a long long int: 8 byte(s)\n
-		Size of a float: 4 byte(s)");
+	printf("Size of a char: %u byte(s)", sizeof(char));
+	printf("Size of an int: %u byte(s)", sizeof(int));
+	printf("Size of a long int: %u byte(s)", sizeof(long));
+	printf("Size of a long long int: %u byte(s)", sizeof(long double));
+	printf("Size of a float: %u byte(s)", sizeof(float));
 	return (0); }
