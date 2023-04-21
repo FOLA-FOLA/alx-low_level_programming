5e728fe3e8e970e4ccb46623a1834f7b02d8e9b4
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-20 20:34:54 -0700

    Create file 6 make 5,6 executable

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ * main - prints out the size of each type
+ *
+ * Return: This function always returns 0
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n
+		Size of an int: 4 byte(s)\n
+		Size of a long int: 4 byte(s)\n
+		Size of a long long int: 8 byte(s)\n
+		Size of a float: 4 byte(s)");
+	return (0); }
