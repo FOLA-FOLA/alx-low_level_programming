97147ad3af93a96e312026ddc10702bcdd422a75
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-20 20:11:21 -0700

    Make file 5 betty compliant

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This function printfs dummy text
+ *
+ * Return: This function returns 0 always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); }
