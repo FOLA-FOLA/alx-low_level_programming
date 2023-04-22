61dd100f0391eef50a868d82ba63830f802e5c68
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-21 19:42:40 -0700

    Create file 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - This function checks and returns if n is greater than 5
+ * or equal to zero or less than 6 and prints the result.
+ *
+ * Return: This function returns a value of 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	char last = "Last digit of";
+
+	if (n > 5)
+		printf("%s is %i and is greater than 5\n", last, n);
+	else if (n == 0)
+		printf("%s is %i and is zero\n", last, n);
+	else
+		printf("%s is %i and is less than 6 and not 0\n", last, n);
+	return (0);
+}
