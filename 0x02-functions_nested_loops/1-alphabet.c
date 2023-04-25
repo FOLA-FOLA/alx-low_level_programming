f091f386310231dd24bc5cb44d407eddd4c2a4df
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-25 14:16:45 -0700

    Make gcc and betty complliant

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_alphabet - prints out the alphabet
+  *
+  * Return: v0id
+  */
+void print_alphabet(void)
+{
+	char flt;
+
+	for (flt = 'a'; flt <= 'z'; flt++)
+	{
+		_putchar(flt);
+	}
+	_putchar('\n');
+}
