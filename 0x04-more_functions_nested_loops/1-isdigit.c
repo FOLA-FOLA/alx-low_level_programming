c8b8c36b63b61c92e32f6c1ff113befd6f800566
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-27 10:32:54 -0700

    create c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _isdigit - Function checks if parameter
+ * is a digit from or between 0 - 9
+ *
+ * @c: This is the charcter that is being checked for digit
+ *
+ * Return: This function returns 1 if condition is true
+ * and 0 if otherwise.
+ */
+int _isdigit(int c)
+{
+	int i;
+	int tmp = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == c)
+			tmp += 1;
+	}
+	if (tmp > 0)
+		return (1);
+	else
+		return (0);
+}
