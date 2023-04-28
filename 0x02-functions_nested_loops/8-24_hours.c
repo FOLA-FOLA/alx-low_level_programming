b5623333e30144dcb54e04d297115768b65edfde
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-28 05:53:16 -0700

    Create function to print every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauer - This function prints all the minutes of the day
+ *
+ * Return: This function returns 0
+ */
+void jack_bauer(void)
+{
+	int j,k,l,m;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (i == 2 && j > 3)
+					{
+						break;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
