ba313d2fa3e8165f188bb7ad375b86f09ccf3f1e
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-28 06:15:18 -0700

    Write code to print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times table - This function prints the 9 times table, 
+ * starting from 0.
+ *
+ * Return: This function returns 0
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0, a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(c + 48);
+			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
