3073367c8fca91d6b88c0c89a3e56c7ab9b68c06
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-28 08:35:48 -0700

    Create function that prints 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - This function prints the numbers from 0 - 9,
+ * followed by a new line.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; 1 <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
