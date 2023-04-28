4ee5e7eaa854726ea3c42466d07fde6e59b8a408
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-28 06:31:15 -0700

    Create c function that adds two ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - This function add two integers and returns
+ * the value thereof
+ *
+ * @a: First int parameter to be added.
+ * @b: Second int parameter to be added.
+ *
+ * Return: This function returns the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
