6c58ef5e5957a0152915b44f26083245e677ab6d
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-28 05:15:35 -0700

    Write code to compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - Prints the absolute value of an integer
+ *
+ * @a: The integer whose absolute value is being calculated
+ *
+ * Return: This function returns 0
+ */
+int _abs(int a)
+{
+	int k = a;
+	int ab;
+
+	if (k < 0)
+	{
+		k *= -1;
+		return (k);
+	}
+	else
+	{
+		return (k);
+	}
+}
