ccd2dc7b0baa2c37ae39fc2c72257ede9803e8ba
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-28 12:41:37 -0700

    Create file

---
+++ 0x04-more_functions_nested_loops/4-print_more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - This function prints all numbers
+ * from 0 - 9 except 2 and 4
+ *
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar(i);
+	}
+	_putchar('\n');
+}
