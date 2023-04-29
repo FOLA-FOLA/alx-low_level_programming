545fe7777cdeea9b789003ceeb6b37f92c6aa95e
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-29 11:24:42 -0700

    Create file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - This function prints 10 times the numbers
+ * from 10 to 14, followed by a new line.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 1; i <= 14; i++)
+	{
+		if (i < 10)
+		{
+			_putchar(i + '0');
+		}
+		else
+		{
+			_putchar('1' + (i % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
