485d7da90803715b575ab4dd1fbc6f3d8df03454
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-29 12:16:48 -0700

    Rewrite function 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
- * print_most_numbers - This function prints all numbers
- * from 0 - 9 except 2 and 4
+ * print_most_numbers - This function prints the
+ * numbers 0 - 9, followed by a newline.
  *
  * Return: Void
  */
 void print_most_numbers(void)
 {
-	int i;
-
 	for (i = 48; i <= 57; i++)
 	{
-		if (i != '2' || i != '4')
+		if (i != 50 || i = 52)
 		{
 			_putchar(i);
 		}
