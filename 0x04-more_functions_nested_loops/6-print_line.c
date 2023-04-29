aae994748180ec1cbb7f2d874f0e6db8367ba06c
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-29 12:28:48 -0700

    Remove if looop

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,16 +12,7 @@ void print_line(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		if (n <= 0)
-		{
-			_putchar('\\');
-			_putchar('n');
-		}
-		else
-		{
-			_putchar('_');
-		}
+			_putchar(95);
 	}
-	_putchar('\\');
-	_putchar('n');
+	_putchar('\n');
 }
