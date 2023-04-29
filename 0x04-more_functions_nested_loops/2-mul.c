79d81438cea4e3bfce53e56f45b89535c877efde
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-29 00:54:04 -0700

    Change return to a*b

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -9,30 +9,5 @@
  */
 int mul(int a, int b)
 {
-	int  m = a * b;
-	int t;
-
-	if (m < 9)
-	{
-		_putchar(m);
-	}
-	else if (m > 9 && m < 100)
-	{
-		_putchar((m / 10) - ((m % 10) * 0.1));
-		_putchar(m % 100);
-	}
-	else if (m > 99 && m < 1000)
-	{
-		_putchar((m / 100) - ((m % 100) * 0.01));
-		_putchar((m % 100) - ((m % 10) * 0.1));
-		_putchar(m % 10);
-	}
-	else
-	{
-		_putchar((m / 1000) - ((m % 1000) * 0.001));
-		_putchar((m % 1000) - ((m % 100) * 0.01));
-		_putchar((m % 100) - ((m % 10) * 0.1));
-		_putchar(m % 10);
-	}
-	return (0);
+	return (a * b);
 }
