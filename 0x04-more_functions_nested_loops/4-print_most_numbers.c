4fc834a2d878d2d9fc566b97a5ff6ae494210064
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-29 01:01:55 -0700

    Remove continue

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,8 @@ void print_most_numbers(void)
 	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
-		if (i == 50 || i == 52)
-			continue;
+		if (i != 50 || i != 52)
+			_putchar(i);
 	}
 	_putchar('\n');
 }
