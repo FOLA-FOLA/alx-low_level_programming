b6a9e29e365261bddb31a43f1b8f0335b1b6efc9
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-04-29 13:16:44 -0700

    Create file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print all single digit base 10 number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
