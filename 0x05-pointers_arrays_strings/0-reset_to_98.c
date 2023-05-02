68bb3da7bd97c3779837f59b097ca94849d3a64b
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-02 09:13:30 -0700

    Create readme, main.h and file 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - This function takes a pointar to an int as parameter
+ * and updates the value that it points to, to 98
+ *
+ * @n: This pointar should have its pointed to value updated
+ * to 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+This directory contains c files.
+These c files are for learning about pointers, arrays and strings
+These files and the projects attached contribute towards my learning
