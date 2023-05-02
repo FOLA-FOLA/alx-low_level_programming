a7a46fc0052d06990462681617e494eac437df78
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-02 12:05:46 -0700

    Create file _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - This function returns the length of the string
+ *
+ * @s: This is the string whose characters is to be counted
+ *
+ * Return: integer, the length of the string parameter.
+ */
+int _strlen(char *s)
+{
+	int a = *s;
+
+	return (a);
+}
