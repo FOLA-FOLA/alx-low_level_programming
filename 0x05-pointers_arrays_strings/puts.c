89cb1ba9ba042bb9b77dcb40e39aba83b1592877
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-02 19:56:23 -0700

    Create function _puts

---
+++ 0x05-pointers_arrays_strings/puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts1 -This function prints a string, followed
+ * by a newline to stdout
+ *
+ * @str: This is the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '/0')
+	{
+		_putchar(s);
+		str++;
+	}
+	_putchar('\n')
+}
