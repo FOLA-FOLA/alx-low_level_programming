530b208897bbee1c70b4d782b27b882d8c8b9660
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-04 00:05:24 -0700

    Find file 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 /**
- * main - This function prints all the single digit base 10
- * followed by a new line
+ * main - This function prints all the hexadecimal
+ * numbers in lowercase
  *
  * Return: 0
  */
 int main(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
+	}
+	for (j = 97; j <= 102; i++)
+	{
+		putchar(j);
 	}
 	putchar('\n');
 	return (0);
