2e608744f00bb2fb3d6a93ad81cddd7b8df7ea20
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-05 05:34:41 -0700

    Create function 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - concatenated two strings up to the 
+ * nth character
+ *
+ * @src: string to be concatenated
+ * @dest: string to be concatenated to.
+ * @n: concatenation should stop at the nth
+ * character of src.
+ *
+ * Return: @dest, the newly concatenated string.
+ */
+char *_strncat(char *dest, char *_src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*src != '\0')
+	{
+		i += 1;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+}
