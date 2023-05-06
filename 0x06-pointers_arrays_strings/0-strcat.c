65a89294dd5a2ccabb841e275185004b4dddb367
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-05 19:09:43 -0700

    Write function based on man page

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,25 @@
 #include "main.h"
 /**
- * _strncat - concatenated two strings up to the
- * nth character
+ * _strcat - concatenated two strings.
  *
  * @src: string to be concatenated
  * @dest: string to be concatenated to.
- * @n: concatenation should stop at the nth
- * character of src.
  *
  * Return: @dest, the newly concatenated string.
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	
+	int j;
+
+	while (*(dest + i) != '\0')
+	{
+		i += 1;
+	}
+	for (j = 0; src[j] != '\0'; j += 1)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
 }
