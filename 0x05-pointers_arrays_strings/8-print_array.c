9822da8d845955948ebd1851c09006cefcb5a774
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-08 10:41:20 -0700

    Add if loop to for loop

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%i, ", a[i]);
+		if (i != n - 1)
+			printf("%i, ", a[i]);
+		else
+			printf("%i\n", a[n - 1]);
 	}
-	printf("%i\n", a[n - 1]);
 }
