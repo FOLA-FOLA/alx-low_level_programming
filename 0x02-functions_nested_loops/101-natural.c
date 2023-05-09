bba1571f63cb886aa686d8bb915dffbe1fbfb578
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-09 00:41:59 -0700

    Correction to loop conditionals

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 /**
  * main - This function prints the sum of
- * all the natural numbers below 1024 that 
+ * all the natural numbers below 1024 that
  * are multiples of 3 and 5.
  *
  * Return: void.
  */
-void main(void)
+int main(void)
 {
 	int i;
 	int sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if (i % 5 == 0 && i % 3 == 0)
-			sum +=  i;
+		if (i % 5 == 0 || i % 3 == 0)
+			sum += i;
 		else
 			continue;
 	}
 	printf("%d\n", sum);
+	return (0);
 }
