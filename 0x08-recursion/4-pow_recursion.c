80ef69f71bdf82eb1528cd11864af1a95882d9dc
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-10 20:55:31 -0700

    Write function for factorial

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - This function returns x raised
+ * to the power of y
+ *
+ * @x: Number to be raised to the power of y
+ * @y: The power of x
+ *
+ * Return: An integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
