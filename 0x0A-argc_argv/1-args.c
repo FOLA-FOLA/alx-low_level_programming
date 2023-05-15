0d8f91c30b8792844acc9766b3f6b548e8db91fa
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-15 10:33:49 -0700

    Write function that prints num of args passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _prtcount - This function prints the number of 
+ * arguments that a function has
+ *
+ * @argc: This is the number to be printed.
+ *
+ * Return: void
+ */
+void prtcount(int argc)
+{
+	printf("%i\n", argc);
+}
