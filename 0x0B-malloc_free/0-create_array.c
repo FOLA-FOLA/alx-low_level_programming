7954b8b2db1de072c09add98fb38a40b34e632ae
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-16 13:02:17 -0700

    Write function strdup

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,19 +12,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
+	char *arr;
 	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	array = (char *)malloc(sizeof(char) * size);
-	if (array == NULL)
+	arr = (char *)malloc(sizeof(char) * size);
+	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i += 1)
 	{
-		array[i] = c;
+		arr[i] = c;
 	}
-	return (array);
+	return (arr);
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - This function duplicates a string.
+ *
+ * @str: This is the string to be duplicated.
+ *
+ * Return: a char; A copy of @str above.
+ */
+char *_strdup(char *str)
+{
+	char srn;
+	char *a;
+	int len;
+
+	while (str[len] != '\0')
+	{
+		len += 1;
+	}
+	p = srn;
+	while (*(str + 1) != len)
+	{
+		*a++ = *srn++
+	}
+	*a = '\0';
+	return (srn);
+}
