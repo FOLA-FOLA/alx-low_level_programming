4e50bfb135e478aa2fc7108b948d51225ef64eee
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-17 13:45:14 -0700

    Add main function

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _prtcount - This function prints the number of 
+ * main - This function prints the number of 
  * arguments that a function has
  *
  * @argc: This is the number to be printed.
  *
  * Return: void
  */
-int prtcount(int argc)
+int main(int argc)
 {
 	printf("%i\n", argc);
 	return (0);
