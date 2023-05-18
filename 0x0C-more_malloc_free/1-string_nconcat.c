5f4a7c8aa0b34b10c8e28bb92e04f1abf5145312
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-18 16:15:03 -0700

    Add 1 for null terminator

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1[len + i] = s2[i];
 	}
 	s1[len + i] = '\0';
-	a = malloc(sizeof(char) * (len + n));
+	a = malloc(sizeof(char) * (len + n + 1));
 	if (a == NULL)
 		return (NULL);
 	return (a);
