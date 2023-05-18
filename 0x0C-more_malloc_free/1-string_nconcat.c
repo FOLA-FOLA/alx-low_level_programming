d97653b010523af34275a3dc0a985c842f813f3d
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-18 16:40:26 -0700

    Create lenstr function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * lenstr - Return the length of a string.
+ *
+ * @s: The string to be indexed.
+ *
+ * Return: An int.
+ */
+int lenstr(char *s)
+{
+	int a;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
+/**
  * string_nconcat - This function concatenates two
  * string and returns a pointer to
  * the allocated space in memory.
@@ -17,21 +34,18 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int le, len = 0;
-	unsigned int i;
+	int len = 0;
+	unsigned int i, le;
 	char *a;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (num < 0)
-		return (NULL);
-	le = _strlen(s2);
+	le = lenstr(s2);
 	if (n >= le)
 		n = le;
-	while (s1[len] != '\0')
-		len += 1;
+	len = lenstr(s1);
 	for (i = 0; s2[i] != '\0' && i < n; i++)
 	{
 		s1[len + i] = s2[i];
