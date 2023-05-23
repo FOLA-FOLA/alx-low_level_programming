70e10526572c76079d9bbc2bc45566a429f92535
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-23 14:25:03 -0700

    eliminate betty for abs value"

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-/**
- * x - This is the absolute
- * value of a number with help from macros
- *
- * Return: 0
- */
+
 #define ABS(x) ((x) > (0) ? (x) : -(x))
 
 #endif
