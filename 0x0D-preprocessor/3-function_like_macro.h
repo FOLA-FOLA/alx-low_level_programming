62479cf87d57f2542833519cf467b23f13247395
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-23 14:17:26 -0700

    Add proper documentation

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 /**
- * main - This function prints the absolute
+ * x - This is the absolute
  * value of a number with help from macros
  *
  * Return: 0
