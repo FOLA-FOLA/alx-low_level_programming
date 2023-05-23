44b2304ee338dbcf725d8dfb7427799e3cdb3e41
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-23 13:54:05 -0700

    Write function to print _FILE_

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - This function prints out
+ * the name of the file it was compiled
+ * from, followed by a new line.
+ * - By Ogunseiju Folakemi.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
