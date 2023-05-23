4f5c45d957d171ebefc3922cd242da93a9b2fd02
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-23 14:13:35 -0700

    Write header file for task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * main - This function prints the absolute
+ * value of a number with help from macros
+ *
+ * Return: 0
+ */
+#define ABS(x) ((x) > (0) ? (x) : -(x))
+
+#endif
