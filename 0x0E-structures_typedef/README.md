8bb803f6377dc60ce97464931f2f82fb220141f0
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-23 21:49:56 -0700

    Write struct for dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This directory contains files about the C language.
+These files contribute to my learning of Structs and Typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - This is a struct containing
+ * valuable personal information about
+ * a metaphorical dog.
+ *
+ * @name: This is the name the dog bears
+ * @age: The age of the dog
+ * @owner: The name of the dear owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
