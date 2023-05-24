d992e4b4e224c4bbe82c8033a41fa62e6c191332
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-24 01:24:00 -0700

    Write function for array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_iterator - This function executes a function
+ * given as a parameter on each element of an array.
+ *
+ * @array: This is the array to be iterated upon
+ * @size: This is the size of the said array above
+ * @action: This is a pointer to the function i need to use.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(*array);
+		}
+	}
+}
