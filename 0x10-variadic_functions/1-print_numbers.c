c8cd3cdddfbd4b85ab9063bd691ce0029bf543c0
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-05-25 15:22:22 -0700

    Change the condition for the if statement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		separator = "";
 	}
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (i < n - 1)
 			printf("%u%s", va_arg(p, unsigned int), separator);
