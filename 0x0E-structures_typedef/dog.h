b94f19c1aed3f47541635611045221f7ecf0be1e
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-05 11:20:40 -0700

    Add dog_t as a new name for struct dog.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - This is a struct containing
  * valuable personal information about
@@ -7,13 +11,19 @@
  *
  * @name: This is the name the dog bears
  * @age: The age of the dog
- * @owner: The name of the dear owner
- */
-struct dog
+ * @owner: The name of the dear owner */
+
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
