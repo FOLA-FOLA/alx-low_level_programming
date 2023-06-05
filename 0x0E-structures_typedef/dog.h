ef2c189209e566d75ccb143b090da9639a1ba760
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-05 11:26:28 -0700

    Make dog.h betty compliant

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,7 +11,8 @@
  *
  * @name: This is the name the dog bears
  * @age: The age of the dog
- * @owner: The name of the dear owner */
+ * @owner: The name of the dear owner.
+ */
 
 
 typedef struct dog
@@ -26,4 +27,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
