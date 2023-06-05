9a4c1c0a7e3fbd45835743f179dd577b414a10e9
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-05 13:13:54 -0700

    Free name and owner if null

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,23 +6,33 @@
  * @name: This isactually the dog's name.
  * @age: This is apparently the dog's age.
  * @owner: This refers to teh woner of the dog.
+ *
+ * Return: Pointer to the new dog.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *n, *ow;
 
-	dog_t *nwd = malloc(sizeof(dog_t));;
+	dog_t *nwd = malloc(sizeof(dog_t));
 
 	n = strdup(name);
+	if (n  == NULL)
+	{
+		free(nwd);
+		return (NULL);
+	}
 	ow = strdup(owner);
+	if (ow == NULL)
+	{
+		free(n);
+		free(nwd);
+		return (NULL);
+	}
 
 	nwd->name = n;
 	nwd->age = age;
 	nwd->owner = ow;
 
-
-	if (n == NULL && ow == NULL)
-		return NULL;
 	return (nwd);
 
 }
