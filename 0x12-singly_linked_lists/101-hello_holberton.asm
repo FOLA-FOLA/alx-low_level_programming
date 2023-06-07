1abe53774adb94a5febf59e0d1610387abcf6cab
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-07 10:33:59 -0700

    Creare asm file

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_hol - This function prints
+ * hello, holberton followed by a newline.
+ *
+ */
+
+void print_hol(void)
+{
+	printf("Hello, Holberton\n");
+}
