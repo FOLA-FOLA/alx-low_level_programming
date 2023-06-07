40a4842862fc0f58ab3e7819a6f8d041119820aa
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-07 14:57:28 -0700

    Write function pointers to pick operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,11 +1,26 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct op - struct for op
+ *
+ * @op: Operator for use.
+ * @f: Function related to operator
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
-int op_mult(int a, int b);
+int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_up_func(char *s))(int, int);
+int (*get_op_func(char *s))(int, int);
 
 #endif
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,3 @@
-#include "functions_pointers.h"
 #include "3-calc.h"
 #include <stdio.h>
 /**
@@ -22,19 +21,19 @@ int op_add(int a, int b)
  *
  * Return: A int which is the difference of the two ints.
  */
-int op_sub(int a, intb)
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_mult - This function multiplies two ints
+ * op_mul - This function multiplies two ints
  *
  * @a: INT 1
  * @b: INTEGER 2
  *
  * Return: Product of a and b
  */
-int op_mult(int a, int b)
+int op_mul(int a, int b)
 {
 	return (a * b);
 }
@@ -47,8 +46,13 @@ int op_mult(int a, int b)
  *
  * Return: An int, dividing a by b
  */
-int op_mult(int a, int b)
+int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 /**
@@ -62,5 +66,10 @@ int op_mult(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
