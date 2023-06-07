b8a364858ed788285a09032f97fb286f2a5de82e
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-07 10:41:34 -0700

    Write in assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,27 @@
-#include "lists.h"
-#include <stdio.h>
+section .data
+    message db 'Hello, Holberton!', 0
+    newline db 10
 
-/**
- * print_hol - This function prints
- * hello, holberton followed by a newline.
- *
- */
+section .text
+    global _start
 
-void print_hol(void)
-{
-	printf("Hello, Holberton\n");
-}
+_start:
+    ; Write the message to stdout
+    mov eax, 4                   ; sys_write system call
+    mov ebx, 1                   ; file descriptor (stdout)
+    mov ecx, message             ; address of the message
+    mov edx, 13                  ; length of the message
+    int 0x80                     ; invoke the kernel
+
+    ; Write a new line to stdout
+    mov eax, 4                   ; sys_write system call
+    mov ebx, 1                   ; file descriptor (stdout)
+    mov ecx, newline             ; address of the new line
+    mov edx, 1                   ; length of the new line
+    int 0x80                     ; invoke the kernel
+
+    ; Exit the program
+    mov eax, 1                   ; sys_exit system call
+    xor ebx, ebx                 ; exit status (0)
+    int 0x80                     ; invoke the kernel
+
