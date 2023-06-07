123795bec4c54a82fd69f66c12ce3e913c34fbae
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-07 15:30:00 -0700

    Function print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,58 +1,50 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - Print all arguments.
- *
- * @format: specify operation.
- *
+ * print_all - Prints all of the args when specified
+ * @format: specifies the necessary operations
  * Return: void
  */
+
 void print_all(const char * const format, ...)
 {
-	int a;
-	int fl;
-	char *s;
+	int i;
+	int flag;
+	char *str;
 	va_list a_list;
 
 	va_start(a_list, format);
-	a = 0;
-	while (format != NULL && format[a] != '\0')
+	i = 0;
+	while (format != NULL && format[i] != '\0')
 	{
-		if (format[a] == 'c')
+		switch (format[i])
 		{
-			printf("%c", va_arg(a_list, int));
-			fl = 0;
-			break;
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
 		}
-		else if (format[a] == 'i')
-		{
-			printf("%i", va_arg(a_list, int));
-			fl = 0;
-			break;
-		}
-		else if (format[a] == 'f')
-		{
-			printf("%f", va_arg(a_list, double));
-			fl = 0;
-			break;
-		}
-		else if(format[a] == 's')
-		{
-			s = va_arg(a_list, char *);
-			if (s == NULL)
-				s = "(nil)";
-			printf("%s", s);
-			fl = 0;
-			break;
-		}
-		else
-		{
-			fl = 1;
-			break;
-		}
-		if (format[a + 1] != '\0' && fl == 0)
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
-		a++;
+		i++;
 	}
 	printf("\n");
 	va_end(a_list);
