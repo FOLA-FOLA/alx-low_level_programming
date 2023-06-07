2ebe48f58b22c749dceabc1745588340e1cdcd82
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-07 08:11:28 -0700

    Write function to check prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * ck - Check if number is prime.
+ *
+ * @a: An integer
+ * @g: An integer
+ *
+ * Return: An integer
+ */
+int ck(int c, int g)
+{
+	if (g < 2 || g % c == 0)
+		return (0);
+	else if (c > g / 2)
+		return (1);
+	else
+		return (ck(c + 1, g));
+}
+/**
+ * is_prime_number - return if the number is prime.
+ *
+ * @n The number to be checked.
+ *
+ * Return: Integer.
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (ck(2, n));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <unistd.h>
 #include <stdio.h>
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int _sqrt_recursion(int n);
 int ck(int c, int g);
+int ck(int c, int g);
+int is_prime_number(int n);
+
+#endif
