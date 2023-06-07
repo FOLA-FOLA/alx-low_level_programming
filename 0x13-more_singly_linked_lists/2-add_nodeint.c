7599deaf7071f685181f5495ead5681a9565fa74
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-06 20:28:15 -0700

    Add checker to check if malloc failed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,8 +13,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *p = malloc(sizeof(listint_t));
 
+	if (p == NULL || head == NULL)
+	{
+		return (NULL);
+	}
 	p->n = n;
 	p->next = *head;
+	*head = p;
 
 	return (p);
 }
