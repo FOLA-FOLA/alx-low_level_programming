9d8a077bb1c03da3fcc249be2b40c8783f568145
FOLA-FOLA <ogunseijufolakemi@gmail.com>
2023-06-07 15:06:51 -0700

    Add va_end for variadic function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(p, n);
 
-	if (separator != NULL)
+	if (separator == NULL)
 	{
 		separator = "";
 	}
@@ -30,4 +30,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 	}
 	printf("\n");
+	va_end(p);
 }	
