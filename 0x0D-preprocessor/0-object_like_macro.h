facd9cbe37b07dd1dc50030f7263b3aa2ab05e24
Folakemi Ogunseiju <112573318+FOLA-FOLA@users.noreply.github.com>
2023-06-30 09:33:01 +0100

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,8 +0,0 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-/**
- * SIZE - This file defines a macro
- * named SIZE.
- */
-#define SIZE 1024
-#endif
--- 0x0D-preprocessor/README.md
+++
@@ -1,2 +0,0 @@
-This file contains files on C and C preprocessor.
-This is a part of my learning of the C language.
