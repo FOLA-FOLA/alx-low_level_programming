0919b19a6c9e84b21079e4b4160840c91c895dbf
fola-fola <ogunseijufolakemi@gmail.com>
2023-07-11 07:43:36 -0700

    Fix segementation fault

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,12 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	int i;
 
+	if (size == 0)
+		return (NULL);
+
 	p = malloc(size);
 
-	if (p == NULL && size == 0)
+	if (p == NULL)
 	{
 		return (NULL);
 	}
